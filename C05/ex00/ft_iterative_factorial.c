bde0e08db00b2c902a14aff2d7f3aa531297d8c4
Muhammad Anas Majeed Rafique <mumajeed@cbr5s3.42barcelona.com>
2024-08-28 14:08:07 +0200

    done

---
+++ C05/ex00/ft_iterative_factorial.c
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_iterative_factorial.c                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mumajeed <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/08/28 12:58:43 by mumajeed          #+#    #+#             */
+/*   Updated: 2024/08/28 13:04:48 by mumajeed         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int	ft_iterative_factorial(int nb)
+{
+	int	i;
+
+	i = nb;
+	if (nb < 0)
+		return (0);
+	if (nb == 0)
+		return (1);
+	while (i > 1)
+	{
+		nb = nb * (i - 1);
+		i--;
+	}
+	return (nb);
+}
+/*#include <stdio.h>
+
+int	main(void)
+{
+	printf("%i", ft_iterative_factorial(3));
+}*/
