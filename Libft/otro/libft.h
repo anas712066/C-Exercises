dc4e13920ad80813282bef7899d393cfdc63660d
Muhammad Anas Majeed Rafique <mumajeed@car2s3.42barcelona.com>
2024-09-28 19:19:06 +0200

    done

--- Libft/otro/libft.h
+++ Libft/otro/libft.h
@@ -17,7 +17,6 @@
 # include <string.h>
 # include <unistd.h>
 # include <stdlib.h>
-# include <stddef.h>
 
 int		ft_isalnum(int c);
 int		ft_isdigit(int c);
@@ -28,5 +27,6 @@ size_t	ft_strlen(const char *s);
 void	ft_bzero(void *s, size_t n);
 void	*ft_memset(void *s, int c, size_t n);
 void	*ft_memcpy(void *dest, const void *src, size_t n);
+void	*ft_memmove(void *dest, const void *src, size_t n);
 
 #endif
